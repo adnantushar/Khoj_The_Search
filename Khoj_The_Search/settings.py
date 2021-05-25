4277b66ae59038ebf64f2371f28e57e19351ed00
adnan.tushar <adnan.tushar@gmail.com>
2021-05-25 22:39:12 +0600

    Change settings static

--- Khoj_The_Search/settings.py
+++ Khoj_The_Search/settings.py
@@ -138,9 +138,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
